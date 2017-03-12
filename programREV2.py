96eab2fd86f4c4119eb3a40da761e52b2cf42468
RobertShultz <rshultz3@gmail.com>
2017-03-11 20:34:36 -0800

    3/11/2017 Updated with hitograms
    ...

--- programREV2.py
+++ programREV2.py
@@ -9,7 +9,7 @@ import numpy.linalg as LA
 from pandas.tools.plotting import scatter_plot
 from matplotlib.pyplot import scatter
 
-def load_mnist(dataset="training", digits=range(10), path=r"C:\Users\Robert Shultz\Google Drive\Machine Learning\Assignment 3\ImageData"):
+def load_mnist(dataset="training", digits=range(10), path=r"C:\Users\Robert\Documents\GitHub\Assignment-3\ImageData"):
     """
     Adapted from: http://cvxopt.org/applications/svm/index.html?highlight=mnist
     """
@@ -47,11 +47,21 @@ T = labels
 
 """X is the trainging data"""
 """T is the class labels"""
-
 # """This is a test of the matplotlib print"""
 # plt.imshow(X[1].reshape(28, 28),interpolation='None', cmap=cm.gray )
 # show()
 
+def Build2DHistograms(X,T,B, xmin, xmax):
+    Hn = np.zeros([B,B]).astype('int32')
+    Hp = np.zeros([B,B]).astype('int32')
+    RC = clip(around(np.round(((B-1)*(X-xmin)/(xmax-xmin)))).astype('int32'),0,B-1)
+    for i,rc in enumerate (RC):
+        if T[i]==2:
+            Hn[rc[0],rc[1]]+=1
+        else:
+            Hp[rc[0],rc[1]]+=1
+    data = [Hn,Hp]
+    return data
 
 def xzcvpr(X):
     u=np.mean(X,axis=0)
@@ -59,38 +69,41 @@ def xzcvpr(X):
     C=np.cov(Z,rowvar=False)
     [L,V]=LA.eigh(C)
     L=np.flipud(L)
-    V=np.flipud(V.T)  
+    V=np.flipud(V.T)
     P = np.dot(Z,V.T)
     P=P[:,:2]
     P = np.append(P,np.zeros([len(P),1]),1)
-#     P[:,2]=T[:,0]
-    print((P[:6,:]))
-    print((T[:6,:]))
-    P[:,2]=P[:,2]+T[:,0]
-    print((P[:6,:])) 
+    P[:,2]=P[:,2]+T[:,0] 
     Pn = P[(P[:,2]==2)]
     Pp = P[(P[:,2]==3)]
-    print(Pn[:5,:])
-    print(Pp[:5,:])
-    plt.scatter(Pn[:,0],Pn[:,1],color='red', s = 1)
-    plt.scatter(Pp[:,0],Pp[:,1],color='blue', s = 1)
-    plt.show()
+    mup=np.mean(Pp,axis=0)[:2]
+    mun=np.mean(Pn,axis=0)[:2]  
+    P1min = amin(P[:,0])
+    P1max = amax(P[:,0])
+    P2min = amin(P[:,1])
+    P2max = amax(P[:,1])  
     
-#     Pn = P[P[:,2]==2]
-#     print(P[4,:])
-#     print(unique(labels))
-#     print(Pn[50,:])
-#     R = np.dot(P,V)
-#     scatter(P[:,0],P[:,1], s=5)
-#     plt.show()
-
+    Pmax = amax(P[:,:2])
+    Pmin = amin(P[:,:2])
+       
+    plt.scatter(Pn[:,0],Pn[:,1],color='red', s = 10)
+    plt.scatter(Pp[:,0],Pp[:,1],color='blue', s = 10)
+    xlabel("P1")
+    ylabel("P2")
+    plt.plot([mup[0],mup[0]],[P2min,P2max],linewidth=5)
+    plt.plot([mun[0],mun[0]],[P2min,P2max],color='red',linewidth=5)
+    plt.plot([P1min,P1max],[mup[1],mup[1]],color='blue',linewidth=5)
+    plt.plot([P1min,P1max],[mun[1],mun[1]],color='red',linewidth=5)
+    plt.show()
+    cn = np.cov(Pn[:,:2].T, ddof = 1)
+    cp = np.cov(Pp[:,:2].T, ddof = 1)
+    B = np.round(np.log2(len(P))+1,0)
+    print(B)
+    print(Build2DHistograms(P[:,:2],P[:,2] , B, Pmin, Pmax)[0])
+    print('\n',Build2DHistograms(P[:,:2],P[:,2] , B, Pmin, Pmax)[1])
 
 xzcvpr(X)
 
-
-
-
-# #     [print(a) for a in P[:,:]]
 #     hmin = amin(P[:,0])
 #     hmax = amax(P[:,0])
 #     vmin = amin(P[:,1])
